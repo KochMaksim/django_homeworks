b472f8d2e42c4fefb685842f8e688387eff83038
KochMaksim <kochergamax@gmail.com>
2022-12-01 20:42:01 +0300

    homework_2.1

--- 2.1-databases/models_list_displaying/main/settings.py
+++ 2.1-databases/models_list_displaying/main/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'gjcnuhtc_w',
+        'PASSWORD': '',
     }
 }
 
--- 2.1-databases/work_with_database/main/settings.py
+++ 2.1-databases/work_with_database/main/settings.py
@@ -55,7 +55,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'gjcnuhtc_w',
+        'PASSWORD': '',
     }
 }
 
